a391c4ccb2ca712a13fcffccad562e8f59bb7af2
Olga Papkovskaya <o.papkovskaya@gmail.com>
2017-01-31 03:38:52 +0300

    final solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -30,23 +30,39 @@ class TicTacToe {
     }
 
     getWinner() { // should return winner symbol (`x` or `o`) or null if there is no winner yet
-        if ((this.matrix[0][0] == this.matrix[0][1] == this.matrix[0][2] != null) ||
-            (this.matrix[1][0] == this.matrix[1][1] == this.matrix[1][2] != null) ||
-            (this.matrix[2][0] == this.matrix[2][1] == this.matrix[2][2] != null)) return this.player;
-        else
-            if ((this.matrix[0][0] == this.matrix[1][0] == this.matrix[2][0] != null) ||
-                (this.matrix[0][1] == this.matrix[1][1] == this.matrix[2][1] != null) ||
-                (this.matrix[0][2] == this.matrix[1][2] == this.matrix[2][2] != null)) return this.player;
-        else
-            if ((this.matrix[0][0] == this.matrix[1][1] == this.matrix[2][2] != null) ||
-                (this.matrix[0][2] == this.matrix[1][1] == this.matrix[2][0] != null)) return this.player;
-        else
-            return null;
+        if ((this.matrix[0][0] == this.matrix[0][1]) && (this.matrix[0][1]  == this.matrix[0][2])
+            && (this.matrix[0][0] != null) && (this.matrix[0][1] != null)&& (this.matrix[0][2] != null))
+            return this.matrix[0][0];
+        else if ((this.matrix[1][0] == this.matrix[1][1]) && (this.matrix[1][1]== this.matrix[1][2])
+            && (this.matrix[1][0] != null) && (this.matrix[1][1] != null)&& (this.matrix[1][2] != null))
+            return this.matrix[1][0];
+        else if ((this.matrix[2][0] == this.matrix[2][1]) && (this.matrix[2][1] == this.matrix[2][2])
+            && (this.matrix[2][0] != null) && (this.matrix[2][1] != null)&& (this.matrix[2][2] != null))
+            return this.matrix[2][0];
+        else if ((this.matrix[0][0] == this.matrix[1][0]) && (this.matrix[1][0] == this.matrix[2][0])
+            && (this.matrix[0][0] != null) && (this.matrix[1][0] != null)&& (this.matrix[2][0] != null))
+            return this.matrix[0][0];
+        else if ((this.matrix[0][1] == this.matrix[1][1]) && (this.matrix[1][1] == this.matrix[2][1])
+            && (this.matrix[0][1] != null) && (this.matrix[1][1] != null)&& (this.matrix[2][1] != null))
+            return this.matrix[0][1];
+        else if ((this.matrix[0][2] == this.matrix[1][2]) && (this.matrix[1][2] == this.matrix[2][2])
+            && (this.matrix[0][2] != null) && (this.matrix[1][2] != null)&& (this.matrix[2][2] != null))
+            return this.matrix[0][2];
+        else if ((this.matrix[0][0] == this.matrix[1][1]) && (this.matrix[1][1] == this.matrix[2][2])
+            && (this.matrix[0][0] != null) && (this.matrix[1][1] != null)&& (this.matrix[2][2] != null))
+            return this.matrix[0][0];
+        else if ((this.matrix[0][2] == this.matrix[1][1]) && (this.matrix[1][1] == this.matrix[2][0])
+            && (this.matrix[0][2] != null) && (this.matrix[1][1] != null)&& (this.matrix[2][0] != null))
+            return this.matrix[0][2];
+        else return null;
     }
 
     noMoreTurns() { // should return true if there is no more fields to place a `x` or `o`
-        for (element in this.matrix){
-            if (element == null) return false;
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++){
+                if (this.matrix[i][j] == null)
+                    return false;
+            }
         }
         return true;
     }
